bbbf54cfab19ff58de3ab931a6ebc577c7162b93
Subhransu Mohapatra <submohap4@WKMZTD7E8020.global.publicisgroupe.net>
2021-08-04 09:23:20 +0530

    initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angularapp';
+export class AppComponent implements OnInit {
+  title = 'angular app';
+  subForm: FormGroup;
+  dataArr = [
+    { id: 1, name: 'data' },
+    { id: 2, name: 'data2' },
+  ];
+
+  ngOnInit() {
+    this.subForm = new FormGroup({
+      name: new FormControl('ddd'),
+      check: new FormControl(true),
+      selection: new FormControl(''),
+    });
+  }
+
+  submitForm(form) {
+    console.log(form);
+  }
 }
